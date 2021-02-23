3d14c420f985b584a5841866dc8620463a329c79
Tharuni11 <79441156+Tharuni11@users.noreply.github.com>
2021-02-23 03:14:27 -0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ABC'
-copyright = '2021, Harideep'
-author = 'Harideep'
+project = 'Github'
+copyright = '2021, Tharuni'
+author = 'Tharuni'
 
 # The full version, including alpha/beta/rc tags
 release = '2021'
